e60cb72160af39a85e657ccc19a1726ad4f6388d
Michiel Jacobs <jacobs.michiel@icloud.com>
2025-10-09 21:45:22 +0200

    prep for production

--- quoteboard/settings.py
+++ quoteboard/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-65n+g%-yi4(aavcr3w2kb+u!pd)l$22mhca=#*ku_o@s)qt$(y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
