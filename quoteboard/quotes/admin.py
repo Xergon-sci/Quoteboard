52704064872816d730e7d4b672a9ff03b9366856
Michiel Jacobs <jacobs.michiel@icloud.com>
2025-12-07 21:13:40 +0100

    added code to admin

--- quoteboard/quotes/admin.py
+++ quoteboard/quotes/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Board, Quote
 
-admin.site.register(Board)
+
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ("name", "code", "date", "pin")
+    readonly_fields = ("code",)
+
+
+admin.site.register(Board, BoardAdmin)
 admin.site.register(Quote)
